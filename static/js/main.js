04f4dbaab792e37345e02ada556bd8ad577368dc
Maximilian Hils <git@maximilianhils.com>
2013-02-21 16:00:15 +0100

    web ui: add first analyze step

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,62 @@
-function search(url) {
+$(function(){
   
-}
+  /*
+  
+  Analysis UX Flow:
+  
+  User enters URL and clicks "Analyze" ->
+  Perform search for existing results and show (Re)submit button ->
+  Request gets resubmitted, page polls for updates ->
+  page notifies user as soon as results are present
+  
+  */
+  
+  var analysisResults = $("#analysis-results"),
+      analysisTable = $("#analysis-table"),
+      analyzeStep1 = $("#analyze-step1"),
+      analyzeStep1ButtonIcon = analyzeStep1.find("button i"),
+      analyzeStep2 = $("#analyze-step2"),
+      analyzeStep2Button = analyzeStep2.find("button"),
+      lazyRecaptchaInit = false;
+  
+  
+  analyzeStep1.submit(function(){
+    
+    //Search for existing results
+    analyzeStep1ButtonIcon.addClass("icon-spin");
+    $.post("/api/search", $(this).serialize(), "json").then(function(data){
+      analyzeStep1ButtonIcon.removeClass("icon-spin");
+      
+      var hasResults = (data.results.length > 0);
+      
+      //hide/show analysis table if we have results
+      hasResults ? analysisResults.show("fast") : analysisResults.hide("fast");
+      if(hasResults) {
+        console.log("FIXME: display results");
+        analysisTable.html($("<pre>").text(JSON.stringify(data.results, null, "  ")));
+        
+      }
+      analyzeStep2.show();
+    });
+    
+    //Lazy init Recaptcha
+     if(!lazyRecaptchaInit){
+       $.getScript("http://www.google.com/recaptcha/api/js/recaptcha_ajax.js").then(function(){
+         Recaptcha.create("6Ld4iQsAAAAAAM3nfX_K0vXaUudl2Gk0lpTF3REf", //FIXME: insert correct key
+                          "analyze-recaptcha", {
+                            theme: "white",
+                            callback: Recaptcha.focus_response_field
+                          });
+       });
+       lazyRecaptchaInit = true;
+     }
+    
+    return false;
+  });
+  
+  
+  analyzeStep2.submit(function(){
+    $.post("/api/submit", $(this).serialize(), "json").then(function(data){
+      console.log("FIXME: Submit");
+  });
+});
--- thugme.py
+++ thugme.py
@@ -3,7 +3,7 @@ from wsgiref.simple_server import make_server
 import bottle
 from bottle import Bottle, static_file, request, redirect
 from jinja2 import Environment, FileSystemLoader
-
+from recaptcha.client import captcha
 
 bottle.debug(True)
 app = Bottle()
@@ -24,6 +24,25 @@ def favicon():
 def thugme():
     template = template_env.get_template('index.html')
     return template.render()
+
+@app.post('/api/search')
+def search():
+    return { "request": request.forms.get('url'),
+             "results": (({"timestamp":123},{"timestamp":456}) if request.forms.get('url') != "no" else [])
+        }
+
+@app.post('/api/analyze')
+def analyze():
+    response = captcha.submit(
+        request.forms.get('recaptcha_challenge_field'),
+        request.forms.get('recaptcha_response_field'),
+        "FIXME: private key",
+        request.environ.get('REMOTE_ADDR')
+        )
+    if not response.is_valid:
+        return {"success": False}
+    else:
+        return {"success": True} #return some id, too.
 
 if __name__ == "__main__":
     httpd = make_server('', 8000, app)
