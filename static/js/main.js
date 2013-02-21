6f31fca41c1b065b23a8cd08c390d6c3efb0a3fa
Maximilian Hils <git@maximilianhils.com>
2013-02-21 16:28:39 +0100

    add captcha error

--- static/js/main.js
+++ static/js/main.js
@@ -11,13 +11,16 @@ $(function(){
   
   */
   
-  var analysisResults = $("#analysis-results"),
-      analysisTable = $("#analysis-table"),
-      analyzeStep1 = $("#analyze-step1"),
+  var analysisResults        = $("#analysis-results"),
+      analysisTable          = $("#analysis-table"),
+      analyzeStep1           = $("#analyze-step1"),
+      analyzeStep1Url        = analyzeStep1.find("input[name=url]"),
       analyzeStep1ButtonIcon = analyzeStep1.find("button i"),
-      analyzeStep2 = $("#analyze-step2"),
-      analyzeStep2Button = analyzeStep2.find("button"),
-      lazyRecaptchaInit = false;
+      analyzeStep2           = $("#analyze-step2"),
+      analyzeStep2Url        = analyzeStep2.find("input[name=url]"),
+      analyzeStep2Button     = analyzeStep2.find("button[type=submit]"),
+      analyzeStep2CaptchaErr = $("#analyze-step2-captchaerror"),
+      lazyRecaptchaInit      = false;
   
   
   analyzeStep1.submit(function(){
@@ -40,23 +43,33 @@ $(function(){
     });
     
     //Lazy init Recaptcha
-     if(!lazyRecaptchaInit){
-       $.getScript("http://www.google.com/recaptcha/api/js/recaptcha_ajax.js").then(function(){
-         Recaptcha.create("6Ld4iQsAAAAAAM3nfX_K0vXaUudl2Gk0lpTF3REf", //FIXME: insert correct key
-                          "analyze-recaptcha", {
-                            theme: "white",
-                            callback: Recaptcha.focus_response_field
-                          });
-       });
-       lazyRecaptchaInit = true;
-     }
+    if(!lazyRecaptchaInit){
+      $.getScript("http://www.google.com/recaptcha/api/js/recaptcha_ajax.js").then(function(){
+        Recaptcha.create("6Ld4iQsAAAAAAM3nfX_K0vXaUudl2Gk0lpTF3REf", //FIXME: insert correct key
+                         "analyze-recaptcha", {
+                           theme: "white",
+                           callback: Recaptcha.focus_response_field
+                         });
+      });
+      lazyRecaptchaInit = true;
+    }
+    
+    //copy over url to second form
+    analyzeStep2Url.val(analyzeStep1Url.val());
     
     return false;
   });
   
   
   analyzeStep2.submit(function(){
-    $.post("/api/submit", $(this).serialize(), "json").then(function(data){
-      console.log("FIXME: Submit");
+    $.post("/api/analyze", $(this).serialize(), "json").then(function(data){
+      if(!data.success) {
+        analyzeStep2CaptchaErr.slideDown("fast");
+        Recaptcha.reload();
+      } else {
+      	console.log("FIXME: Submit");
+      }
+    });
+    return false;
   });
 });
